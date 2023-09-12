6d2923c8d77d735ffd306e61e205bd38036f81a1
priyesh777 <priyeshgautam1010@gmail.com>
2023-09-12 18:23:03 -0400

    First commit for data management

---
+++ script.js
@@ -0,0 +1,3 @@
+console.log('This is the first simple task.');
+console.log('Data management is believed to have the core concepts for any mobile app development.');
+console.log('It is an exciting course for any individual');

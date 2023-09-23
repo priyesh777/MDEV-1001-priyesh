62ed658fe8b7a9a9d96cc6c76d46fcf117c3353d
priyesh777 <priyeshgautam1010@gmail.com>
2023-09-22 23:01:52 -0400

    Add movies sql file

---
+++ ICE2DB.session.sql
@@ -0,0 +1,80 @@
+INSERT INTO movieData (
+    title,
+    studio,
+    genres,
+    directors,
+    writers,
+    actors,
+    year,
+    length,
+    shortDescription,
+    mpaRating,
+    criticsRating
+  )
+VALUES (
+    'Star Trek II: The Wrath of Khan',
+    'Paramount Pictures',
+    'Action, Adventure, Sci-fi',
+    'Nicholas Meyer',
+    'Gene Roddenberry, John Doe, Jim Rohn',
+    'William Shatner, Leonard Nimoy, DeForest Kelley',
+    '1982',
+    113,
+    'With the assistance of the Enterprise crew, Admiral Kirk must stop an old nemesis, Khan Noonien Singh, from using the life-generating Genesis Device as the ultimate weapon.',
+    'PG',
+    '7.7'
+  );
+
+INSERT INTO movieData (
+    title,
+    studio,
+    genres,
+    directors,
+    writers,
+    actors,
+    year,
+    length,
+    shortDescription,
+    mpaRating,
+    criticsRating
+  )
+VALUES (
+    'Star Wars: Episode V - The Empire Strikes Back',
+    'Twentieth Century Fox',
+    'Action, Adventure, Sci-fi',
+    'Ivrin Kershner',
+    'Leigh Brackett, Lawrence Kasdan, George Lucas',
+    'Mark Hamill, Harrison Ford, Carrie Fisher',
+    '1980',
+    124,
+    'After the Rebels are overpowered by the Empire, Luke Skywalker begins his Jedi training with Yoda, while his friends are pursued across the galaxy by Darth Vader and bounty hunter Boba Fett.',
+    'PG',
+    '8.7'
+  );
+
+  INSERT INTO movieData (
+    title,
+    studio,
+    genres,
+    directors,
+    writers,
+    actors,
+    year,
+    length,
+    shortDescription,
+    mpaRating,
+    criticsRating
+  )
+VALUES (
+    'Back to the Future',
+    'Universal Pictures',
+    'Action, Adventure, Sci-fi',
+    'Robert Zemeckis',
+    'Robert Zemeckis, Bob Ghale',
+    'Michael J. Fox, Christopher Lloyd, Lea Thompson',
+    '1985',
+    116,
+    'Marty McFly, a 17-year-old high school student, is accidentally sent 30 years into the past in a time-traveling DeLorean invented by his close friend, the maverick scientist Doc Brown.',
+    'PG',
+    '8.5'
+  );
